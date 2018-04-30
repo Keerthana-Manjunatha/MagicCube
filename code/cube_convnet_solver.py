cc133105cc1ab42e07a40db059fbcdb4deb12454
Jon Guan <jonathan.guan@capitalone.com>
2018-04-29 23:15:47 -0700

    old pycuber works

--- code/cube_convnet_solver.py
+++ code/cube_convnet_solver.py
@@ -44,6 +44,169 @@ The canonical order is found by doing
 After any rotation, this can be used to quickly restore the cube to
 canonical position.
 """
+class Cube:
+    """Magic Cube Representation"""
+    # define some attribues
+    default_plastic_color = 'black'
+    default_face_colors = ["w", "#ffcf00",
+                           "#00008f", "#009f0f",
+                           "#ff6f00", "#cf0000",
+                           "gray", "none"]
+    base_face = np.array([[1, 1, 1],
+                          [1, -1, 1],
+                          [-1, -1, 1],
+                          [-1, 1, 1],
+                          [1, 1, 1]], dtype=float)
+    stickerwidth = 0.9
+    stickermargin = 0.5 * (1. - stickerwidth)
+    stickerthickness = 0.001
+    (d1, d2, d3) = (1 - stickermargin,
+                    1 - 2 * stickermargin,
+                    1 + stickerthickness)
+    base_sticker = np.array([[d1, d2, d3], [d2, d1, d3],
+                             [-d2, d1, d3], [-d1, d2, d3],
+                             [-d1, -d2, d3], [-d2, -d1, d3],
+                             [d2, -d1, d3], [d1, -d2, d3],
+                             [d1, d2, d3]], dtype=float)
+
+    base_face_centroid = np.array([[0, 0, 1]])
+    base_sticker_centroid = np.array([[0, 0, 1 + stickerthickness]])
+
+    # Define rotation angles and axes for the six sides of the cube
+    x, y, z = np.eye(3)
+    rots =  []
+    angles_x = [np.pi/2, -np.pi/2]
+
+    for theta in angles_x:
+        rots +=[Quaternion.from_v_theta(x, theta)]
+    angles_y = [np.pi / 2, -np.pi / 2, np.pi, 2 * np.pi]
+
+    for theta in angles_y:
+        rots += [Quaternion.from_v_theta(y, theta)]
+
+    facesdict = dict(F=z, B=-z,
+                     R=x, L=-x,
+                     U=y, D=-y)
+
+    def __init__(self, N=3, plastic_color=None, face_colors=None):
+        self.N = N
+        if plastic_color is None:
+            self.plastic_color = self.default_plastic_color
+        else:
+            self.plastic_color = plastic_color
+
+        if face_colors is None:
+            self.face_colors = self.default_face_colors
+        else:
+            self.face_colors = face_colors
+
+        self._move_list = []
+        self._initialize_arrays()
+
+
+    def _initialize_arrays(self):
+        # initialize centroids, faces, and stickers.  We start with a
+        # base for each one, and then translate & rotate them into position.
+
+        # Define N^2 translations for each face of the cube
+        cubie_width = 2. / self.N
+        translations = np.array([[[-1 + (i + 0.5) * cubie_width,
+                                   -1 + (j + 0.5) * cubie_width, 0]]
+                                 for i in range(self.N)
+                                 for j in range(self.N)])
+
+        # Create arrays for centroids, faces, stickers, and colors
+        face_centroids = []
+        faces = []
+        sticker_centroids = []
+        stickers = []
+        colors = []
+
+        factor = np.array([1. / self.N, 1. / self.N, 1])
+
+        for i in range(6):
+            M = self.rots[i].as_rotation_matrix()
+            faces_t = np.dot(factor * self.base_face
+                             + translations, M.T)
+            stickers_t = np.dot(factor * self.base_sticker
+                                + translations, M.T)
+            face_centroids_t = np.dot(self.base_face_centroid
+                                      + translations, M.T)
+            sticker_centroids_t = np.dot(self.base_sticker_centroid
+                                         + translations, M.T)
+            colors_i = i + np.zeros(face_centroids_t.shape[0], dtype=int)
+
+            # append face ID to the face centroids for lex-sorting
+            face_centroids_t = np.hstack([face_centroids_t.reshape(-1, 3),
+                                          colors_i[:, None]])
+            sticker_centroids_t = sticker_centroids_t.reshape((-1, 3))
+
+            faces.append(faces_t)
+            face_centroids.append(face_centroids_t)
+            stickers.append(stickers_t)
+            sticker_centroids.append(sticker_centroids_t)
+            colors.append(colors_i)
+
+        self._face_centroids = np.vstack(face_centroids)
+        self._faces = np.vstack(faces)
+        self._sticker_centroids = np.vstack(sticker_centroids)
+        self._stickers = np.vstack(stickers)
+        self._colors = np.concatenate(colors)
+
+        self._sort_faces()
+
+    def _sort_faces(self):
+        # use lexsort on the centroids to put faces in a standard order.
+        ind = np.lexsort(self._face_centroids.T)
+        self._face_centroids = self._face_centroids[ind]
+        self._sticker_centroids = self._sticker_centroids[ind]
+        self._stickers = self._stickers[ind]
+        self._colors = self._colors[ind]
+        self._faces = self._faces[ind]
+
+    def rotate_face(self, f, n=1, layer=0):
+        """Rotate Face"""
+        if layer < 0 or layer >= self.N:
+            raise ValueError('layer should be between 0 and N-1')
+
+        try:
+            f_last, n_last, layer_last = self._move_list[-1]
+        except:
+            f_last, n_last, layer_last = None, None, None
+
+        if (f == f_last) and (layer == layer_last):
+            ntot = (n_last + n) % 4
+            if abs(ntot - 4) < abs(ntot):
+                ntot = ntot - 4
+            if np.allclose(ntot, 0):
+                self._move_list = self._move_list[:-1]
+            else:
+                self._move_list[-1] = (f, ntot, layer)
+        else:
+            self._move_list.append((f, n, layer))
+
+        v = self.facesdict[f]
+        r = Quaternion.from_v_theta(v, n * np.pi / 2)
+        M = r.as_rotation_matrix()
+
+        proj = np.dot(self._face_centroids[:, :3], v)
+        cubie_width = 2. / self.N
+        flag = ((proj > 0.9 - (layer + 1) * cubie_width) &
+                (proj < 1.1 - layer * cubie_width))
+
+        for x in [self._stickers, self._sticker_centroids,
+                  self._faces]:
+            x[flag] = np.dot(x[flag], M.T)
+        self._face_centroids[flag, :3] = np.dot(self._face_centroids[flag, :3],
+                                                M.T)
+    def _pycuber_rep (self,pycuber_cube):
+        self._pycuber_rep = pycuber_cube.copy()
+
+    def draw_interactive(self):
+        fig = plt.figure(figsize=(5, 5))
+        fig.add_axes(InteractiveCube(self))
+        return fig
+
 
 
 class InteractiveCube(plt.Axes):
